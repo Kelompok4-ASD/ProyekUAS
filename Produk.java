5165086d204bc0c8db27cc680038e62926f6a00c
dapaptraa <2510631250012@student.unsika.ac.id>
2026-05-14 13:13:44 +0700

    Add files via upload

---
+++ Produk.java
@@ -0,0 +1,35 @@
+public class Produk {
+
+    public int     id;
+    public String  nama;
+    public String  kategori;
+    public int     stok;
+    public double  harga;
+    public boolean aktif; // false = soft delete
+
+    public Produk(int id, String nama, String kategori, int stok, double harga) {
+        this.id       = id;
+        this.nama     = nama;
+        this.kategori = kategori;
+        this.stok     = stok;
+        this.harga    = harga;
+        this.aktif    = true; // default aktif saat dibuat
+    }
+
+    public String hargaFormatted() {
+        String s = Long.toString((long) harga);
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (count > 0 && count % 3 == 0) sb.insert(0, '.');
+            sb.insert(0, s.charAt(i));
+            count++;
+        }
+        return "Rp" + sb.toString();
+    }
+
+    public static String truncate(String s, int max) {
+        if (s.length() <= max) return s;
+        return s.substring(0, max - 2) + "..";
+    }
+}
