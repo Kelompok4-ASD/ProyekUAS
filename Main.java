9333dd349991ffd2fd290be13f4a17348d2e6c4a
Shinta2647 <2510631250048@student.unsika.ac.id>
2026-05-21 20:16:59 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,11 @@
 import java.util.Scanner;
 
-
 public class Main {
-
-
    static Scanner sc = new Scanner(System.in);
-
-
+   
    public static void main(String[] args) {
 
-
        StokManager.initDataAwal();
-
-
        int pilihan;
        do {
            System.out.println("\n|==============================================|");
@@ -70,46 +63,3 @@ public class Main {
 }
 
 
-
-
-
-Produk.java
-public class Produk {
-
-
-   public int     id;
-   public String  nama;
-   public String  kategori;
-   public int     stok;
-   public double  harga;
-   public boolean aktif; // false = soft delete
-
-
-   public Produk(int id, String nama, String kategori, int stok, double harga) {
-       this.id       = id;
-       this.nama     = nama;
-       this.kategori = kategori;
-       this.stok     = stok;
-       this.harga    = harga;
-       this.aktif    = true; // default aktif saat dibuat
-   }
-
-
-   public String hargaFormatted() {
-       String s = Long.toString((long) harga);
-       StringBuilder sb = new StringBuilder();
-       int count = 0;
-       for (int i = s.length() - 1; i >= 0; i--) {
-           if (count > 0 && count % 3 == 0) sb.insert(0, '.');
-           sb.insert(0, s.charAt(i));
-           count++;
-       }
-       return "Rp" + sb.toString();
-   }
-
-
-   public static String truncate(String s, int max) {
-       if (s.length() <= max) return s;
-       return s.substring(0, max - 2) + "..";
-   }
-}
